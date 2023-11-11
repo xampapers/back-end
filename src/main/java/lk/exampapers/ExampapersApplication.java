21620350469bfeaa62c451b8ae4129b48bdeeda3
Subhash Chamara <udayanga.subhash94@gmail.com>
2023-11-11 20:41:08 +0530

    Initialize the project

---
+++ src/main/java/lk/exampapers/ExampapersApplication.java
@@ -0,0 +1,13 @@
+package lk.exampapers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampapersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampapersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/exampapers/ExampapersApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.exampapers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampapersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
